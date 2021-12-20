ae2899f069f02c20b99d8a5272b5b409b9d2f56b
C <95203358+antillean-nighthawk@users.noreply.github.com>
2021-12-20 13:25:56 -0800

    Kaeya and Albedo
    - added link to source

--- voice_lines.py
+++ voice_lines.py
@@ -1,4 +1,5 @@
-
+# file contains the voice lines of each character
+# sourced from https://genshin-impact.fandom.com/wiki/Category:Character_Voice-Overs
 
 hu_tao_lines = [
                 "Yoh, now why might you be looking for me, hm? Oh, you didn't know? I'm the 77th Director of the Wangsheng Funeral Parlor, Hu Tao. Though by the looks of you... Radiant glow, healthy posture... Yes, you're definitely here for something other than that which falls within my regular line of work, aren't you?",
@@ -7,12 +8,12 @@ hu_tao_lines = [
                 "One client, two clients, three clients!",
                 "When the sun's out, bathe in sunlight. But when the moon's out, bathe in moonlight~"
                 ],
+                [
                 "*sigh* What a bummer...",
                 "Lightning! Let's bolt!",
                 "Snow!",
-                [
                 "Ahhhh~ What a nice breeze~",
-                " My hat's gonna get blown away!"
+                "My hat's gonna get blown away!"
                 ],
                 "Well are those who rise in the early morn, while those late to bed I shall forewarn~",
                 "Yo! Afternoon! Had lunch?",
@@ -62,5 +63,123 @@ hu_tao_lines = [
                 "Yeah, this is good, thanks!",
                 "I never imagined that I, Hu Tao, could ever be like this. Energy like a rushing torrent, a heart soaring like a gliding eagle! I feel great!",
                 "If I reach my full physical potential, then the next test is of the heart. It's largely thanks to you that I've been able to step foot into this uncharted territory. It is here that heart and soul are as one like clouds. Death is a constant for all among the multitudes that sit beneath the heavens."
+                ]
+               ]
+
+kaeya_lines = [
+                "Kaeya, Knight of Favonius. Please, allow me to join you on your journey. Guarding you on your journey sounds far more entertaining than any of the usual Favonius stuff.",
+                [
+                "I didn't know you were such a slacker.",
+                "Hehe, seems all the other knights already have their hands full.",
+                "Come on, let's get moving. We're not frozen in place, after all."
+                ],
+                [
+                "Cryo is always far more effective in the rain. This is going to get interesting...",
+                "How about a ghost story to go with the snow?",
+                "What's wrong? Can't handle the cold?",
+                "What a pleasant breeze, I wonder how long it'll last?",
+                ],
+                "Seize the day.",
+                "Hey, fancy joining me at the tavern for a bit?",
+                "You're not scared of the dark, are you? That's... interesting.",
+                "Leaving already? See you next time then, don't make me wait too long though.",
+                [
+                "Is the Abyss Order causing you trouble? If there's anything bothering you, you know you can talk to me.",
+                "The title of Cavalry Captain is nothing to get excited about. Now that I think about it, the Grand Master took all the cavalry from Mondstadt so there's none left for me to captain... Heh.",
+                "There are a total of ten captains within the Knights of Favonius. But truth be told, not all of them have what it takes to be leaders.",
+                "My eye? My eye is fine. There's nothing unusual about hiding one's body parts from view. It's the same reason I wear pants... or any other item of clothing, for that matter.",
+                "Khaenri'ah, huh? You sure know a lot! The legacy of Khaenri'ah is long gone, the sinners are all that's left, and they're not worth mentioning.",
+                "I didn't realize we'd known each other for so long now. Time flies! You know, following you on your journey might just be the most fun I've had in my whole life. I hope it stays that way."
+                ],
+                [
+                "If you ever need me, let me know. Let's not make this a transaction relationship.",
+                "Oh? Well, if you're concerned about keeping things professional... maybe you can find more use for me on the front lines. How 'bout it boss? Haha, just don't feel intimidated by what I can do."
+                ],
+                "Still struggling with how Visions work? Let's see... How can I explain it... The apparatus that allows us to see is our eyes. A Vision is the apparatus by which we control the elements. Simple, is it not?",
+                "Do you drink wine? Dandelion wine has a unique body to it. Oh, you're underage? You should try Mondstadt's apple cider vinegar. I know just the place.",
+                "Have you ever seen the owl of Dragonspine? If you look directly at it, it seems to see right through you, while letting go of none of its own secrets... Quite fascinating, don't you think?",
+                [
+                "What? The Acting Grand Master? You need to give her your full support.",
+                "Oh Lisa. Hmm... You'd better not forget to return your books on time. I made that mistake once and boy was she angry. My arm is still numb from last time...",
+                "That Outrider is a clever and righteous knight. Be careful not to step out of line in front of her.",
+                "Diluc? He was cute as a young lad. But nowadays he just seems to be in a world of his own... Overall, not much fun to hang out with.",
+                "Klee. Seems she has failed to elude the Acting Grand Master's investigative skills, despite the pointers I gave her... Haha, I'm kidding, no need to go reporting me or anything.",
+                "Albedo, eh? Calm, collected, and incredibly talented. He's the type everybody likes, some even more so than others. What, you into him as well?",
+                "Have you tried one of Diona's drinks? Oh right, you're still underage. Haha, such a shame, it truly is — to not be able to revel with you in such bliss. Hehe, I guess all we can do is wait for you to be old enough.",
+                "Now that you mention him, it is a shame there is no one in the Knights of Favonius to inherit the Grand Master's approach to his work. *sigh* I must say I really do appreciate people like him.",
+                "What? You think Fischl having one eye covered is very fitting given her title of Prinzessin der Verurteilung. Hahaha, if that's the case, that must make me a future royal progeny too, no?",
+                "I was entrusted by the Acting Grand Master to work in the background to support Noelle, but there hasn't been much for me to concern myself with — she has a good head on her shoulders. Quite the relief.",
+                "Ahaha, you want to know about the secrets of Sister Rosaria? That... I cannot tell you. Though my impression of her is not bad — certainly better than a certain someone whose name I won't bother to mention. She's honest with herself. She casts off that which she has no time for and she doesn't feign motivation for affairs that do not interest her.",
+                "Sucrose may not be much of an extrovert in public, but as they say — it's always the quiet ones... Apparently, her laboratory is home to quite the eclectic range of implements and her experiments are more than just intellectually stimulating... All hearsay, of course. You don't seriously think I would go snooping around a fair lady's private quarters, do you?"
+                ],
+                "Wine tasting is an art form. Mondstadt's wine is a gift from the Anemo Archon, it's only right that one should learn how to appreciate it.",
+                "Lonely? Me? Not with someone here nagging me all the time, that's for sure.",
+                [ 
+                "Sparkling wine is a specialty of Mondstadt. Mix it with three parts Dandelion Wine and the bitterness disappears completely. This concoction is known as \"Death After Noon.\" Delectable.",
+                "I can't believe that some people actually drink grape juice... if they had a little more patience, they would find that it ferments into the most exquisite wine. Hmph. So unromantic."
+                ],
+                "Today is a day worth celebrating, I hope this day can bring you true happiness.",
+                [
+                "Hmm. Not bad.",
+                "You always seem to be around to give me a hand. Thanks.",
+                "Hmm, seems I'm getting stronger. I say, how is it you seem happier than me about it?",
+                "We were just traveling partners. I never thought I'd have you to thank for getting me to this point. Seems I really should respond in kind. Let us carry on, partner. The road ahead is still long."
+                ]
+              ]
+
+albedo_lines = [
+                "I am Albedo, Chief Alchemist of the Knights of Favonius. You carry the aura of the stars, interesting... I would like to study you, if you do not mind. I'm certain we will have many opportunities to be alone in the future.",
+                [
+                "What a view... How about a quick break so I can sketch this beautiful scenery?",
+                '"The truth of this world"... *sigh* What could it be?',
+                'Going out into the world and investigating, turning the "unknown" into the "known." ...Ahhh, I missed this feeling.'
+                ],
+                [
+                "The soil is moist, ideal for collecting the roots of Boom Blossoms. No please, allow me to handle this.",
+                "Master...",
+                "Ample light... active biological organisms... Yes, this is a good place to sketch."
+                ],
+                "Good morning. Are we going to search for ancient relics today?",
+                "Good afternoon. I heard your footsteps. My, it certainly is lively outside of the workshop.",
+                "*sigh* It's gotten quiet outside..."
+                "Good night, you go ahead and rest. I will just finish one last experiment before bed... If you are interested in the results, I can discuss them with you tomorrow.",
+                [
+                "Genius? ...A number of people call me that. But I don't think I'm any \"genius.\"",
+                "Relationships are... quite troublesome. Once you establish a relation with someone, you must continue to maintain it; if you lose contact, you must reestablish the relation. This taxing cycle requires a lot of time...",
+                "Have a question for me? Go ahead, ask. It doesn't take too long to ask a single question, does it? I'm about to begin the final stage of an experiment...",
+                "Oh, by the way. Did you know that without human manipulation, you would need to harness the power of a sun eight times the size of our own in order to naturally create gold?",
+                "The time required to sketch portraits closely correlates with one's mood. I could spend half a day sketching Huffman, while I might only need three strokes of the pencil to sketch Kaeya — one for the face, one for the eyepatch string... and one for the eyepatch.",
+                "What do I do besides sketching and research...? I will occasionally read novels from Inazuma and Liyue. They contain many fascinating ideas which can sometimes provide me with unexpected inspirations for my own creations.",
+                "Even now, I find the nigredo processing stage of alchemy passed down by my master to be full of mystery. What did my master want to create using this technique...? My research is far from over, I will require your help to investigate further."
+                ],
+                [
+                "You... want to accompany me while I experiment? I'm honored. Oh, don't be nervous. My experiments may be dangerous, but no one gets hurt... most of the time.",
+                "Would you oblige me by serving as my assistant? After observing so many experiments, you surely know a good deal about alchemy by now. Relax, we will work together. I don't think you will have any problems. I have faith in my ability to instruct you, and even more faith in your exceptional talents."
+                ],
+                "My Vision? Ah, that's nothing more than a tool to aid in my experiments, no different from a beaker or crucible. The one thing about it that really intrigues me is the principle behind how it works... One day, I will uncover its secrets, it's only a matter of time.",
+                "There's something I've always wanted to ask you, but feared rejection and disappointment...Eh-hem... I am very interested in your little floating companion. May I borrow her to investigate for a short while?",
+                "You would like to know more about petrified trees? Erm... Actually, the petrified trees you have come across in domains aren't that ancient. Truly ancient trees usually grow to around one or two miles in height. Oh, and there's a kind of large spider that lives around their roots — if you tie one up in cape jasmine and lemongrass and sprinkle on some cardamom, smoke it for a few hours — you get a fragrant taste that's simply... unforgettable.",
+                [
+                'Family? Although Alice has always asked me to call her mother.... when I think of the idea of "family", my master comes to mind first. My earliest memories in life are of going on adventures with her. She taught me the art of alchemy and much knowledge about the world. Perhaps... even the words "family" and "master" are incapable of fully expressing her meaning to me.',
+                "Sucrose? I think I more or less understand why she is so dedicated to her research in bio-alchemy... Either way, I'm happy to see her succeed more and more in her endeavors.",
+                "Working with Sucrose on alchemy research is quite pleasant, but she has some... stubborn qualities in regards to certain aspects unrelated to research. For example, I have told her numerous times to just call me \"Albedo\" since we are about the same age, but she insists on addressing me in an excessively formal manner. ...Whatever she prefers, I guess.",
+                "Lisa? I always am impressed by the valuable perspectives she brings to discussions. But is a person with her talents satisfied with such a basic occupation as librarian? There must be some hidden reason for this.",
+                "Indeed, I view Klee as my younger sister. Whenever she gets herself into some explosive sort of trouble, I can only console myself with the fact that her current destructiveness is nothing compared to that of Miss Alice.",
+                "Jean? She has served exceptionally well as Acting Grand Master, one can even say she is more reliable than Grand Master Varka. Perhaps in our hearts, we all eagerly await the day she can become the official Grand Master herself.",
+                "Oh, Xingqiu? I find his written works quite interesting. The Yae Publishing House in Inazuma has been asking that I cooperate with a different author for greater royalties. Hmph, do I seem the type to be swayed by a few extra Mora?",
+                "Barbara? She's a very earnest Deaconess. I once had the opportunity to sketch her a simple portrait... What happened to the sketch? Ah yes... Albert offered to purchase it, but I rejected his bid and gave it to the Acting Grand Master. Social relations... really require a lot of effort."
+                ],
+                "Hmm... Besides research, the activity I spend most of my time on is probably sketching. At first I saw it simply as a way to better study the structure of living things and materials so as to aid me in my experiments, but it eventually became a hobby of mine. Indeed, it is a very good way to relax.",
+                "There's just never enough time, is there? Even when I remove the most troublesome matters from my schedule, I still can't find enough time for everything.",
+                [
+                "I really do enjoy having dessert. How can I put it...? When both physical and mental capacity are spent, high-energy materials further provide a kind of primordial, highly-effective boost.",
+                "What food do I dislike? ...Large portions of meat at restaurants. I have a rather small appetite, so a meal like that only ends up in tragedy — either I overeat and the joy of the taste is lost, or I don't finish it and the food is wasted. I decided to simply not go to restaurants, although that has also meant I must spend more time preparing my own food..."
+                ],
+                "Happy birthday. You look especially happy, would you mind if I sketched you? The capacity of our brains is limited, so we are bound to forget things. But when an image is transferred onto paper or canvas, the sketch becomes an extension of our memory. We can remember that past feeling when we later look at the sketch.",
+                [
+                "Nigredo is the first step in alchemy, drawing from primordial forms. As beings who live in this world, we must find our own meaning.",
+                "Albedo is the step in which change begins. Clearing away the excess so we can take on all the knowledge that is available. Would you like to investigate this world with me?",
+                "Rubedo in alchemy refers to the refining of feeling. I feel the refining of my own emotions is also thanks to you.",
+                "Citrinitas is the final stage of the alchemical transmutation process. The meaning of the object being transmuted has finally been brought to light, becoming gold and revealing its true value... I too have found my own meaning."
                 ]
                ]
