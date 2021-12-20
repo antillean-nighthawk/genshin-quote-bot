afac4617290917fe2e4e40ec1aaf663a5d22f714
C <95203358+antillean-nighthawk@users.noreply.github.com>
2021-12-20 13:25:08 -0800

    Kaeya and Albedo
    - changed 'weather' category
    - added make_character function

--- genshin_bot.py
+++ genshin_bot.py
@@ -12,25 +12,32 @@ class Character:
     def __init__(self, voice_lines):
         self.hello = voice_lines[0]
         self.chat = random.choice(voice_lines[1])
-        self.rain = [2]
-        self.thunder = [3]
-        self.snw = [4]
-        self.wind = random.choice(voice_lines[5])
-        self.morning = voice_lines[6]
-        self.afternoon = voice_lines[7]
-        self.evening = voice_lines[8]
-        self.night = voice_lines[9]
-        self.about_self = random.choice(voice_lines[10])
-        self.about_us = random.choice(voice_lines[11])
-        self.vision = voice_lines[12]
-        self.share = voice_lines[13]
-        self.interesting_things = voice_lines[14]
-        self.about_others = random.choice(voice_lines[15])
-        self.hobbies = voice_lines[16]
-        self.troubles = voice_lines[17]
-        self.food = random.choice(voice_lines[18])
-        self.birthday = voice_lines[19]
-        self.ascension = random.choice(voice_lines[20])
+        self.weather = random.choice(voice_lines[2])
+        self.morning = voice_lines[3]
+        self.afternoon = voice_lines[4]
+        self.evening = voice_lines[5]
+        self.night = voice_lines[6]
+        self.about_self = random.choice(voice_lines[7])
+        self.about_us = random.choice(voice_lines[8])
+        self.vision = voice_lines[9]
+        self.share = voice_lines[10]
+        self.interesting_things = voice_lines[11]
+        self.about_others = random.choice(voice_lines[12])
+        self.hobbies = voice_lines[13]
+        self.troubles = voice_lines[14]
+        self.food = random.choice(voice_lines[15])
+        self.birthday = voice_lines[16]
+        self.ascension = random.choice(voice_lines[27])
+
+def make_character(person):
+    if person == 'hu-tao':
+        character = Character(voice_lines.hu_tao_lines)
+    elif person == 'kaeya':
+        character = Character(voice_lines.kaeya_lines)
+    elif person == 'albedo':
+        character = Character(voice_lines.albedo_lines)
+
+    return character
 
 @bot.event
 async def on_ready():
@@ -38,21 +45,14 @@ async def on_ready():
 
 @bot.command()
 async def say(ctx, person, line = None):
-    if person == 'hutao':
-        character = Character(voice_lines.hu_tao_lines)
+    character = make_character(person)
         
     if line == 'hello' or line == None:
         await ctx.send(character.hello)
     elif line == 'chat':
         await ctx.send(character.chat)
-    elif line == 'rain':
-        await ctx.send(character.rain)
-    elif line == 'thunder':
-        await ctx.send(character.thunder)
-    elif line == 'snow':
-        await ctx.send(character.snow)
-    elif line == 'wind':
-        await ctx.send(character.wind)
+    elif line == 'weather':
+        await ctx.send(character.weather)
     elif line == 'morning':
         await ctx.send(character.morning)
     elif line == 'afternoon':
